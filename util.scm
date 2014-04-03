de831d57bfc84e9862377f4394860f85e24e8fed
tkhsh <y-takahashi@timedia.co.jp>
2014-04-04 01:03:51 +0900

    Add util.scm

---
+++ util.scm
@@ -0,0 +1,15 @@
+(define nil ())
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
