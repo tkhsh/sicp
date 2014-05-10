16ccb08d9fd2faca1466b5229f6d8c8c14891714
tkhsh <y-takahashi@timedia.co.jp>
2014-05-11 00:19:25 +0900

    Add ex-2.67.scm

---
+++ ex-2.67.scm
@@ -0,0 +1,13 @@
+(load "./sec-2.3.4.scm")
+
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                   (make-leaf 'B 2)
+                   (make-code-tree (make-leaf 'D 1)
+                                   (make-leaf 'C 1)))))
+
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+(print (decode sample-message sample-tree))
+
