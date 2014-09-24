cc2182571a2b3c74ea8fc4789f3d0c9e96a533e8
tkhsh <y-takahashi@timedia.co.jp>
2014-09-25 01:06:41 +0900

    Add ex-3.3

---
+++ ex-3.3.scm
@@ -0,0 +1,20 @@
+(define (make-account balance password)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch input-password m)
+    (if (eq? input-password password)
+      (cond ((eq? m 'withdraw) withdraw)
+            ((eq? m 'deposit) deposit)
+            (else (error "Unknown request -- MAKE-ACCOUNT"
+                         m)))
+      complain))
+  dispatch)
+
+(define (complain x) "Incorrect password")
+
