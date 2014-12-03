7627934126a2244925984364ff8faa01cb0e04ab
tkhsh <y-takahashi@timedia.co.jp>
2014-12-04 03:34:23 +0900

    Add ex-3.25

---
+++ ex-3.25.scm
@@ -0,0 +1,26 @@
+(define (make-table)
+  (let ((local-table (list #f)))
+    (define (lookup keys table)
+      (if (null? keys)
+        (car table)
+        (let ((record (assoc (car keys) (cdr table))))
+          (if record
+            (lookup (cdr keys) (cdr record))
+            #f))))
+
+    (define (insert! keys value table)
+      (if (null? keys)
+        (set-car! table value)
+        (let ((record (assoc (car keys) (cdr table))))
+          (if record
+            (insert! (cdr keys) value (cdr record))
+            (let* ((t (make-table))
+                   (r (cons (car keys) t)))
+              (set-cdr! table (cons r (cdr table)))
+              (insert! (cdr keys) value t)))))
+      (define (dispatch m)
+        (cond ((eq? m 'lookup-proc) lookup)
+              ((eq? m 'insert-proc!) insert!)
+              (else (error "Unknown operation -- TABLE" m))))
+      dispatch))
+
