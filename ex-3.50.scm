ee01db652a1eacc9d8441fe4ac2f5e7c6b446755
tkhsh <y-takahashi@timedia.co.jp>
2015-02-13 00:19:57 +0900

    ex-3.50: Add new-stream-map

--- ex-3.50.scm
+++ ex-3.50.scm
@@ -1,3 +1,5 @@
+(load "./stream-lib.scm")
+
 (define (stream-map proc . argstreams)
   (if (stream-null? (car argstreams))
       the-empty-stream
@@ -5,3 +7,19 @@
        (apply proc (map stream-car argstreams))
        (apply stream-map
               (cons proc (map stream-cdr argstreams))))))
+
+; improved
+(define (new-stream-map proc . argstreams)
+  (if (any-null-stream? argstreams)
+    the-empty-stream
+    (cons-stream
+      (apply proc (map stream-car argstreams))
+      (apply new-stream-map
+             (cons proc (map stream-cdr argstreams))))))
+
+(define (any-null-stream? streams)
+  (cond ((null? streams)
+         #f)
+        ((stream-null? (car streams))
+         #t)
+        (else (any-null-stream? (cdr streams)))))
