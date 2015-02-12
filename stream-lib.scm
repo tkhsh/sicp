1105f7f30d4e340fcdbac9ce4e4c1d20d2c1a77f
tkhsh <y-takahashi@timedia.co.jp>
2015-02-12 23:49:58 +0900

    Add stream-lib

---
+++ stream-lib.scm
@@ -0,0 +1,54 @@
+(define the-empty-stream '())
+(define stream-null? null?)
+
+(define-syntax cons-stream
+  (syntax-rules ()
+    ((_ <a> <b>)
+     (cons <a> (delay <b>)))))
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc s)
+  (if (stream-null? s)
+      the-empty-stream
+      (cons-stream (proc (stream-car s))
+                   (stream-map proc (stream-cdr s)))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+       low
+       (stream-enumerate-interval (+ low 1) high))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (display-line x)
+  (newline)
+  (display x))
+
+(define (stream-take s n)
+  (if (= n 0)
+    'done
+    (begin (display-line (stream-car s))
+           (stream-take (stream-cdr s) (- n 1)))))
