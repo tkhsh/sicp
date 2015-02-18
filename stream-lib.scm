959399511722e8e39cfe63d3311bd3b3b8a472c8
tkhsh <y-takahashi@timedia.co.jp>
2015-02-18 23:58:18 +0900

    stream-lib: Use stream-map defined in ex-3.50

--- stream-lib.scm
+++ stream-lib.scm
@@ -13,11 +13,25 @@
       (stream-car s)
       (stream-ref (stream-cdr s) (- n 1))))
 
-(define (stream-map proc s)
-  (if (stream-null? s)
-      the-empty-stream
-      (cons-stream (proc (stream-car s))
-                   (stream-map proc (stream-cdr s)))))
+; (define (stream-map proc s)
+;   (if (stream-null? s)
+;       the-empty-stream
+;       (cons-stream (proc (stream-car s))
+;                    (stream-map proc (stream-cdr s)))))
+
+; ex-3.50 new-stream-map
+(define (stream-map proc . argstreams)
+  (if (any stream-null? argstreams)
+    the-empty-stream
+    (cons-stream
+      (apply proc (map stream-car argstreams))
+      (apply stream-map
+             (cons proc (map stream-cdr argstreams))))))
+(define (any pred list)
+  (if (null? list)
+    #f
+    (or (pred (car list))
+        (any pred (cdr list)))))
 
 (define (stream-for-each proc s)
   (if (stream-null? s)
