26ad64caf58e85483bd676851f9db1464149b00e
tkhsh <y-takahashi@timedia.co.jp>
2015-03-19 00:48:54 +0900

    Add ex-3.55

---
+++ ex-3.55.scm
@@ -0,0 +1,9 @@
+(require "./stream-lib.scm")
+
+; ただし、この実装は効率が悪い
+(define (partial-sums stream)
+  (cons-stream (stream-car stream)
+               (add-streams (stream-cdr stream)
+                            (partial-sums stream))))
+
+(display-stream (stream-take (partial-sums integers) 5000))
