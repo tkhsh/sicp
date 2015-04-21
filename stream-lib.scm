061dca1d8bd61b9f20a90d53153dd9092faaec3d
tkhsh <y-takahashi@timedia.co.jp>
2015-04-22 01:49:12 +0900

    stream-lib: Add functions in sec-3.5.3

--- stream-lib.scm
+++ stream-lib.scm
@@ -111,3 +111,40 @@
   (iter primes))
 
 (require "./ex-3.54.scm")
+
+; sec-3.5.3
+(define (sqrt-improve guess x)
+  (average guess (/ x guess)))
+(define (average a b)
+  (/ (+ a b) 2))
+
+(define (sqrt-stream x)
+  (define guesses
+    (cons-stream 1.0
+                 (stream-map (lambda (guess)
+                               (sqrt-improve guess x))
+                             guesses)))
+  guesses)
+
+(define (pi-summands n)
+  (cons-stream (/ 1.0 n)
+               (stream-map - (pi-summands (+ n 2)))))
+(define pi-stream
+  (scale-stream (partial-sums (pi-summands 1)) 4))
+
+(define (euler-transform s)
+  (let ((s0 (stream-ref s 0))           ; Sn-1
+        (s1 (stream-ref s 1))           ; Sn
+        (s2 (stream-ref s 2)))          ; Sn+1
+    (cons-stream (- s2 (/ (square (- s2 s1))
+                          (+ s0 (* -2 s1) s2)))
+                 (euler-transform (stream-cdr s)))))
+
+(define (make-tableau transform s)
+  (cons-stream s
+               (make-tableau transform
+                             (transform s))))
+
+(define (accelerated-sequence transform s)
+  (stream-map stream-car
+              (make-tableau transform s)))
