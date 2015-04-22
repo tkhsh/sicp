b92b1507e627c866310cfdad16c8974ea5b9a7a2
tkhsh <y-takahashi@timedia.co.jp>
2015-04-23 01:16:04 +0900

    ex-3.61: Improve the performance of invert-unit-series

--- ex-3.61.scm
+++ ex-3.61.scm
@@ -3,12 +3,14 @@
 (require "./ex-3.60")
 
 (define (invert-unit-series s)
-  (cons-stream 1
-               (scale-stream (mul-series (stream-cdr s)
-                                         (invert-unit-series s))
-                             -1)))
+  (define x
+    (cons-stream 1
+                 (scale-stream (mul-series (stream-cdr s)
+                                           x)
+                               -1)))
+  x)
 
 (define (main args)
   (display-stream (stream-take (mul-series exp-series
                                            (invert-unit-series exp-series))
-                               5)))
+                               100)))
