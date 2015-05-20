be8328a1fd41b7d1a4a8bbd508856e7ab0f94caa
tkhsh <y-takahashi@timedia.co.jp>
2015-05-21 00:26:55 +0900

    ex-3.72: Add another answer by using stream-group-by

--- ex-3.72.scm
+++ ex-3.72.scm
@@ -11,6 +11,19 @@
                   integers
                   square-weight))
 
+(define (stream-group-by s key-selector)
+  (define (go s key elements)
+    (if (stream-null? s)
+      (cons-stream (cons key elements) the-empty-stream)
+      (let ((k (key-selector (stream-car s))))
+        (if (equal? k key)
+          (go (stream-cdr s) key (cons (stream-car s) elements))
+          (cons-stream (cons key elements)
+                       (go (stream-cdr s) k (list (stream-car s))))))))
+  (if (stream-null? s)
+    the-empty-stream
+    (go (stream-cdr s) (key-selector (stream-car s)) (list (stream-car s)))))
+
 (define (generate-square-sum)
   (define (go s prev count)
     (if (stream-null? s)
@@ -24,6 +37,12 @@
             (go (stream-cdr s) w 1))))))
   (go square-weighted-pairs #f 1))
 
+(define (generate-square-sum)
+  (stream-filter (lambda (g) (= (length (cdr g)) 3))
+                 (stream-group-by square-weighted-pairs square-weight)))
+
 (define (main args)
   (display-stream (stream-take (generate-square-sum)
-                               6)))
+                               6))
+  (display-stream (stream-group-by (stream-enumerate-interval 1 20)
+                                   (lambda (x) (< x 10)))))
