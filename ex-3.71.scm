7c98c41b3896811c89fd4729d8de0f7a4d603323
tkhsh <y-takahashi@timedia.co.jp>
2015-05-20 19:39:56 +0900

    ex-3.71: Improve readablity

--- ex-3.71.scm
+++ ex-3.71.scm
@@ -6,38 +6,38 @@
         (j (cadr pair)))
     (+ (* i i i) (* j j j))))
 
-(define cube-weighted-pairs
+(define ramanujan-weighted-pairs
   (weighted-pairs integers
                   integers
                   ramanujan-weight))
 
-; (define (generate-ramanujan)
-;   (define (_generate-ramanujan s1 s2 dup)
+; (define (generate-ramanujan-numbers)
+;   (define (go s1 s2 dup)
 ;     (if (stream-null? s2)
 ;       the-empty-stream
 ;       (let ((w1 (ramanujan-weight (stream-car s1)))
 ;             (w2 (ramanujan-weight (stream-car s2))))
 ;       (cond ((equal? dup w2)
-;              (_generate-ramanujan (stream-cdr s1) (stream-cdr s2) dup))
+;              (go (stream-cdr s1) (stream-cdr s2) dup))
 ;             ((= w1 w2)
 ;              (cons-stream w1
-;                           (_generate-ramanujan (stream-cdr s1) (stream-cdr s2) w1)))
-;             (else (_generate-ramanujan (stream-cdr s1) (stream-cdr s2) #f))))))
-;   (_generate-ramanujan cube-weighted-pairs (stream-cdr cube-weighted-pairs) #f))
+;                           (go (stream-cdr s1) (stream-cdr s2) w1)))
+;             (else (go (stream-cdr s1) (stream-cdr s2) #f))))))
+;   (go ramanujan-weighted-pairs (stream-cdr ramanujan-weighted-pairs) #f))
 
-(define (generate-ramanujan)
-  (define (_generate-ramanujan s prev count)
+(define (generate-ramanujan-numbers)
+  (define (go s prev count)
     (if (stream-null? s)
       the-empty-stream
       (let ((w (ramanujan-weight (stream-car s))))
         (if (equal? w prev)
-          (if (> count 0)
-            (_generate-ramanujan (stream-cdr s) prev (+ count 1))
+          (if (= count 0)
             (cons-stream w
-                         (_generate-ramanujan (stream-cdr s) prev (+ count 1))))
-          (_generate-ramanujan (stream-cdr s) w 0)))))
-  (_generate-ramanujan cube-weighted-pairs #f 0))
+                         (go (stream-cdr s) prev (+ count 1)))
+            (go (stream-cdr s) prev (+ count 1)))
+          (go (stream-cdr s) w 0)))))
+  (go ramanujan-weighted-pairs #f 0))
 
 (define (main args)
-  (display-stream (stream-take (generate-ramanujan)
+  (display-stream (stream-take (generate-ramanujan-numbers)
                                6)))
