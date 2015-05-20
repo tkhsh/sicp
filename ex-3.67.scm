6efa711ca4ab3260a17ec8869eafacde9fdd4f19
tkhsh <y-takahashi@timedia.co.jp>
2015-05-21 00:29:03 +0900

    ex-3.67: Fix

--- ex-3.67.scm
+++ ex-3.67.scm
@@ -4,7 +4,7 @@
   (cons-stream
     (list (stream-car s) (stream-car t))
     (interleave
-      (stream-map (lambda (x) (list (stream-car t) x))
+      (stream-map (lambda (x) (list x (stream-car t)))
                   (stream-cdr s))
       (pairs-supplement (stream-cdr s) (stream-cdr t)))))
 
@@ -14,4 +14,10 @@
 
 (define (main args)
   (display-stream (stream-take (pairs-whole integers integers)
+                               30))
+  (print "------------------------")
+  (display-stream (stream-take (pairs-supplement integers integers)
+                               10))
+  (print "------------------------")
+  (display-stream (stream-take (pairs integers integers)
                                10)))
