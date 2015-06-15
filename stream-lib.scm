56932986a14468a7b1e3d947d59c2689b598ddf0
tkhsh <y-takahashi@timedia.co.jp>
2015-06-16 08:32:55 +0900

    stream-lib: Add functions from sec-3.55

--- stream-lib.scm
+++ stream-lib.scm
@@ -169,3 +169,38 @@
     the-empty-stream
     (cons-stream (car list)
                  (list->stream (cdr list)))))
+
+;; sec-3.55
+(define random-init 8)
+(define (rand-update x)
+  (define m (expt 2 32))
+  (define a 1664525)
+  (define b 1013904223)
+  (remainder (+ (* a x) b) m))
+
+(define random-numbers
+  (cons-stream random-init
+               (stream-map rand-update random-numbers)))
+
+(define (map-successive-pairs f s)
+  (cons-stream
+   (f (stream-car s) (stream-car (stream-cdr s)))
+   (map-successive-pairs f (stream-cdr (stream-cdr s)))))
+
+(define cesaro-stream
+  (map-successive-pairs (lambda (r1 r2) (= (gcd r1 r2) 1))
+                        random-numbers))
+
+(define (monte-carlo experiment-stream passed failed)
+  (define (next passed failed)
+    (cons-stream
+     (/ passed (+ passed failed))
+     (monte-carlo
+      (stream-cdr experiment-stream) passed failed)))
+  (if (stream-car experiment-stream)
+      (next (+ passed 1) failed)
+      (next passed (+ failed 1))))
+
+(define pi
+  (stream-map (lambda (p) (sqrt (/ 6 p)))
+              (monte-carlo cesaro-stream 0 0)))
