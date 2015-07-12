a7a9418d8efe6ecf4fc61d272a58e64f1fa70e65
tkhsh <y-takahashi@timedia.co.jp>
2015-07-12 23:06:19 +0900

    ex-4.3: Separate env from exp

--- ex-4.3.scm
+++ ex-4.3.scm
@@ -1,63 +1,58 @@
-; envに型をつける必要がある。
-; number?とかばデータ主導につかえないのはなんでだったか？ cf. 2.73 a
-
-; sec-2.4.3
-(define (apply-generic op . args)
-  (let ((type-tags (map type-tag args)))
-    (let ((proc (get op type-tags)))
-      (if proc
-          (apply proc (map contents args))
-          (error
-            "No method for these types -- APPLY-GENERIC"
-            (list op type-tags))))))
-
-; envがいらない場合も受け取るようにしないといけなくなった...
 (define (install-self-evaluating-package)
   (define (eval-self-evaluating exp env)
     exp)
-  (put 'eval '(self-evaluating env) eval-self-evaluating))
+  (put 'eval 'self-evaluating eval-self-evaluating))
 
 (define (install-variable-package)
   (define (eval-variable exp env)
     (lookup-variable-value exp env))
-  (put 'eval '(variable env) eval-variable))
+  (put 'eval 'variable eval-variable))
 
 (define (install-quote-package)
   (define (eval-quote exp env)
     (text-of-quotation exp))
-  (put 'eval '(quote env) eval-quote))
+  (put 'eval 'quote eval-quote))
 
 (define (install-assignment-package)
-  (put 'eval '(assignment env) eval-assignment))
+  (put 'eval 'assignment eval-assignment))
 
 (define (install-definition-package)
-  (put 'eval '(definition env) eval-definition))
+  (put 'eval 'definition eval-definition))
 
 (define (install-if-package)
-  (put 'eval '(if env) eval-if))
+  (put 'eval 'if eval-if))
 
 (define (install-lambda-package)
   (define (eval-lambda exp env)
     (make-procedure (lambda-parameters exp)
                     (lambda-body exp)
                     env))
-  (put 'eval '(lambda env) eval-lambda))
+  (put 'eval 'lambda eval-lambda))
 
 (define (install-begin-package)
   (define (eval-begin exp env)
     (eval-sequence (begin-actions exp) env))
-  (put 'eval '(begin env) eval-begin))
+  (put 'eval 'begin eval-begin))
 
 (define (install-cond-package)
   (define (eval-cond exp env)
     (eval (cond->if exp) env))
-  (put 'eval '(begin env) eval-cond))
+  (put 'eval 'cond eval-cond))
 
 (define (install-application-package)
   (define (eval-application exp env)
     (apply (eval (operator exp) env)
            (list-of-values (operands exp) env)))
-  (put 'eval '(application env) eval-application))
+  (put 'eval 'application eval-application))
 
 (define (eval exp env)
-  (apply-generic 'eval (list exp env)))
+  (let ((exp-type (type-tag exp)))
+    (let ((proc (get 'eval exp-type)))
+      (if proc
+          (proc exp env)
+          (error
+            "No method for this type -- EVAL"
+            exp-type)))))
+; TODO (type-tag exp)を実装する必要がある。
+; データにはタグが必ずついているとは限らないので元々のevalみたいな式の判別処理をする必要がある。
+; ex-2.73のデータ主導版derivは振り分けと処理が分離できていないので中途半端
