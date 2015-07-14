e3fdb7ebab971ff30302c61df6d7f02035bffa1c
tkhsh <y-takahashi@timedia.co.jp>
2015-07-15 00:42:53 +0900

    Answer ex-4.6

---
+++ ex-4.6.scm
@@ -0,0 +1,33 @@
+(define (eval exp env)
+  (cond ((self-evaluating? exp) exp)
+        ((variable? exp) (lookup-variable-value exp env))
+        ((quoted? exp) (text-of-quotation exp))
+        ((assignment? exp) (eval-assignment exp env))
+        ((definition? exp) (eval-definition exp env))
+        ((if? exp) (eval-if exp env))
+        ((lambda? exp)
+         (make-procedure (lambda-parameters exp)
+                         (lambda-body exp)
+                         env))
+        ((begin? exp)
+         (eval-sequence (begin-actions exp) env))
+        ((cond? exp) (eval (cond->if exp) env))
+        ((let? exp) (eval (let->combination exp) env))
+        ((application? exp)
+         (apply (eval (operator exp) env)
+                (list-of-values (operands exp) env)))
+        (else
+          (error "Unknown expression type -- EVAL" exp))))
+
+(define (let? exp)
+  (tagged-list? exp 'let))
+
+(define (let-bindings exp) (cadr exp))
+
+(define (let-body exp) (caddr exp))
+
+(define (let->combination exp)
+  (let ((vars (map car (let-bindings exp)))
+        (vals (map cadr (let-bindings exp))))
+    (cons (make-lambda vars (let-body exp))
+          vals)))
