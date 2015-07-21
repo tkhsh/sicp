8b9a89b7551e8fd3b43e0b58e6065117b42329b8
tkhsh <y-takahashi@timedia.co.jp>
2015-07-22 08:31:44 +0900

    Answer ex-4.8

---
+++ ex-4.8.scm
@@ -0,0 +1,66 @@
+; 1.2.2
+(define (fib n)
+  (define (fib-iter a b count)
+    (if (= count 0)
+      b
+      (fib-iter (+ a b) a (- count 1))))
+  (fib-iter 1 0 n))
+
+; (define (fib-iter a b count)
+;   (if (= count 0)
+;       b
+;       (fib-iter (+ a b) a (- count 1))))
+
+; 4.8
+(define (fib n)
+  (let fib-iter ((a 1)
+                 (b 0)
+                 (count n))
+    (if (= count 0)
+        b
+        (fib-iter (+ a b) a (- count 1)))))
+
+; (let var ((var1 exp1)
+;           (var2 exp2)
+;           ...
+;           (varN expN))
+;   body)
+
+; (define (var var1 var2 ... varN)
+;   body)
+; (var exp1 exp2 ... expN)
+;
+; (let ((var nil))
+;   (set! var (lambda (var1 var2 ... varN) body))
+;   (var exp1 exp2 ... expN))
+
+(letrec ((var (lambda (var1 var2 ... varN) body)))
+  (var exp1 exp2 ... expN))
+
+(define (named-let-bindings exp)
+  (caddr exp))
+
+(define (named-let-var exp)
+  (cadr exp))
+
+(define (named-let-body exp)
+  (cdddr exp))
+
+(define (make-assignment var val)
+  (list 'set! var val))
+
+(define (let->combination exp)
+  (if (list? (cadr exp))
+    (let* ((bindings (let-bindings exp)) ; let
+          (vars (map car bindings))
+          (val-exps (map cadr bindings)))
+     (cons (make-lambda vars (let-body exp))
+           val-exps))
+    (let* ((var (named-let-var exp)) ; named-let
+           (bindings (named-let-bindings exp))
+           (vars (map car bindings))
+           (val-exps (map cadr bindings)))
+      (make-let (list (list var nil))
+                (make-assignment var
+                                 (make-lambda vars (named-let-body exp)))
+                (cons var val-exps)))))
