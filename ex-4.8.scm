bb01b11495e217c7a00121d5f205946fadcf7a5d
tkhsh <y-takahashi@timedia.co.jp>
2015-07-26 22:15:15 +0900

    ex-4.8: Improve readablity

--- ex-4.8.scm
+++ ex-4.8.scm
@@ -34,33 +34,44 @@
 ;   (set! var (lambda (var1 var2 ... varN) body))
 ;   (var exp1 exp2 ... expN))
 
-(letrec ((var (lambda (var1 var2 ... varN) body)))
-  (var exp1 exp2 ... expN))
+; (letrec ((var (lambda (var1 var2 ... varN) body)))
+;   (var exp1 exp2 ... expN))
 
 (define (named-let-bindings exp)
   (caddr exp))
 
-(define (named-let-var exp)
+(define (named-let-name exp)
   (cadr exp))
 
 (define (named-let-body exp)
   (cdddr exp))
+; TODO 場合分けで実装する方法も試す
+; (define (let-bindings exp) (cadr exp))
 
 (define (make-assignment var val)
   (list 'set! var val))
 
+(define (plain-let? exp)
+  (list? (cadr exp)))
+
 (define (let->combination exp)
-  (if (list? (cadr exp))
-    (let* ((bindings (let-bindings exp)) ; let
-          (vars (map car bindings))
-          (val-exps (map cadr bindings)))
-     (cons (make-lambda vars (let-body exp))
-           val-exps))
-    (let* ((var (named-let-var exp)) ; named-let
-           (bindings (named-let-bindings exp))
-           (vars (map car bindings))
-           (val-exps (map cadr bindings)))
-      (make-let (list (list var nil))
-                (make-assignment var
-                                 (make-lambda vars (named-let-body exp)))
-                (cons var val-exps)))))
+  (if (plain-let? exp)
+    (plain-let->combination exp)
+    (named-let->combination exp)))
+
+(define (plain-let->combination exp)
+  (let* ((bindings (let-bindings exp))
+         (vars (map car bindings))
+         (val-exps (map cadr bindings)))
+    (cons (make-lambda vars (let-body exp))
+          val-exps)))
+
+(define (named-let->combination exp)
+  (let* ((name (named-let-name exp))
+         (bindings (named-let-bindings exp))
+         (vars (map car bindings))
+         (val-exps (map cadr bindings)))
+    (make-let (list (list name 'nil))
+              (list (make-assignment name
+                                     (make-lambda vars (named-let-body exp)))
+                    (make-application name val-exps)))))
