de4a0325351486369a09ed1f798a047c3962cd18
tkhsh <y-takahashi@timedia.co.jp>
2015-08-05 01:14:13 +0900

    Refactor - ex-4.10: Rename partition-by-number to group-by-number

--- ex-4.10.scm
+++ ex-4.10.scm
@@ -23,20 +23,20 @@
 ;   body)
 
 (define (let-bindings exp)
-  (partition-by-number 2 (cadr exp)))
+  (group-by-number 2 (cadr exp)))
 
-(define (partition-by-number n lst)
-  (define (go c product rest)
+(define (group-by-number n lst)
+  (define (go c group rest)
     (if (= c 0)
-      (cons product (go n '() rest))
+      (cons group (go n '() rest))
       (if (null? rest)
-        product
+        group
         (go (- c 1)
-            (append product (list (car rest)))
+            (append group (list (car rest)))
             (cdr rest)))))
   (go n '() lst))
 
 (define (main args)
-  ; (print (partition-by-number 2 '(1 2 3 4)))
+  ; (print (group-by-number 2 '(1 2 3 4)))
   (print (let-bindings '(let (var1 exp1 var2 exp2 var3 exp3) body)))
   )
