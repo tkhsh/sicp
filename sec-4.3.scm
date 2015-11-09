651b262fb1bf0563434306ecad2113129381e9ae
tkhsh <y-takahashi@timedia.co.jp>
2015-11-09 23:55:27 +0900

    sec-4.3: Define basic procesures for amb evaluator

--- sec-4.3.scm
+++ sec-4.3.scm
@@ -239,4 +239,26 @@
     try-again
     ))
 
+(define basic-procs
+  '(
+    (define (require p) (if (not p) (amb)))
+    (define (an-element-of items)
+      (require (not (null? items)))
+      (amb (car items) (an-element-of (cdr items))))
+    (define (an-integer-starting-from n)
+      (amb n (an-integer-starting-from (+ n 1))))
+
+    (define (an-integer-between min max)
+      (require (<= min max))
+      (amb min (an-integer-between (+ min 1) max)))
+    )
+  )
+
+(for-each (lambda (exp)
+            (ambeval exp
+                     the-global-environment
+                     (lambda (val next-alternative))
+                     (lambda ())))
+          basic-procs)
+
 ; (pre-eval-driver-loop test)
