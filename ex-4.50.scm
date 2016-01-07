f5c33a320d4373868dc24a2438c5f1398bcf11b5
tkhsh <y-takahashi@timedia.co.jp>
2016-01-08 01:19:55 +0900

    ex-4.50: Show how to help with ex-4.50

--- ex-4.50.scm
+++ ex-4.50.scm
@@ -1,4 +1,4 @@
-(load "./sec-4.3.scm")
+(load "./natural_language.scm")
 (use srfi-27)
 
 (random-source-randomize! default-random-source)
@@ -43,12 +43,72 @@
                       succeed
                       (lambda () (try-random rest)))))))
       (try-random cprocs))))
-
-; (define exps
-;   '(
-;     (ramb 1 2 3 4)
-;     try-again
-;     try-again
-;     try-again
-;     ))
+(define exps
+  '(
+    (ramb 1 2 3 4)
+    try-again
+    try-again
+    try-again
+    ))
 ; (pre-eval-driver-loop exps)
+
+; ex-4.49でAlyssaのアイデアを実装した際には2つの問題があった。
+; 1つ目は常にparse-wordが品詞リストの中を決まった順番で選択する問題
+; 2つ目はparse-wordが常に成功するため
+; phraseを生成する際にmaybe-extendの再起にはまって抜け出せなくなってしまう問題
+;
+; 1つ目の問題に関してはparse-word内のambをrambに変えることで
+; ランダムな順番で品詞を選択することになるため解決できる。
+; 2つ目の問題に関してはランダムで「失敗」も選択するなrambを作成し
+; parse-noun-phraseやphrase-verb-phraseのambをこの新たなrambにすることで解決できる。
+; 「失敗」も選択するようなrambの実装は以下のようなものが考えられる.
+; (define (analyze-ramb exp)
+;   (let ((cprocs (map analyze (ramb-choices exp))))
+;     (lambda (env succeed fail)
+;       (define (try-random choices)
+;         (if (null? choices)
+;           (fail)
+;           (let* ((rand-index (random-integer (+ (length choices) 1))))
+;             (if (= rand-index (length choices))
+;               (fail)
+;               (let ((choice (list-ref choices rand-index))
+;                     (rest   (remove-at choices rand-index)))
+;                 (choice env
+;                         succeed
+;                         (lambda () (try-random rest))))))))
+;       (try-random cprocs))))
+
+(define random-sentence '(
+    (define (parse-word word-list)
+      (define (select-word words)
+        (require (not (null? words)))
+        (ramb (car words)
+              (select-word (cdr words))))
+      (let ((selected-word (select-word (cdr word-list))))
+        (list (car word-list)
+              selected-word)))
+    (define (parse-noun-phrase)
+      (define (maybe-extend noun-phrase)
+        (ramb noun-phrase
+             (maybe-extend
+               (list 'noun-phrase
+                     noun-phrase
+                     (parse-prepositional-phrase)))))
+      (maybe-extend (parse-simple-noun-phrase)))
+
+    (define (parse-verb-phrase)
+      (define (maybe-extend verb-phrase)
+        (ramb verb-phrase
+              (maybe-extend
+                (list 'verb-phrase
+                      verb-phrase
+                      (parse-prepositional-phrase)))))
+      (maybe-extend (parse-word verbs)))
+
+    (parse '())
+    (parse '())
+    (parse '())
+    (parse '())
+    (parse '())
+    ))
+; (pre-eval-driver-loop random-sentence)
