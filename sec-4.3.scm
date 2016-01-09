c92ee977da02cb03b9e537dca1c072adeaac30a9
tkhsh <y-takahashi@timedia.co.jp>
2016-01-09 12:21:54 +0900

    sec-4.3: Add prime-sum-pair?

--- sec-4.3.scm
+++ sec-4.3.scm
@@ -251,6 +251,25 @@
     (define (an-integer-between min max)
       (require (<= min max))
       (amb min (an-integer-between (+ min 1) max)))
+
+    (define (square n) (* n n))
+
+    (define (prime? n)
+      (define (smallest-divisor n)
+        (find-divisor n 2))
+      (define (find-divisor n test-divisor)
+        (cond ((> (square test-divisor) n) n)
+              ((divides? test-divisor n) test-divisor)
+              (else (find-divisor n (+ test-divisor 1)))))
+      (define (divides? a b)
+        (= (remainder b a) 0))
+      (= n (smallest-divisor n)))
+
+    (define (prime-sum-pair list1 list2)
+      (let ((a (an-element-of list1))
+            (b (an-element-of list2)))
+        (require (prime? (+ a b)))
+        (list a b)))
     )
   )
 
