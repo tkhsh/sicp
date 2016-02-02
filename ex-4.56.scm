2e9765ea4c0c3b7a2613df654adad6066e8b098b
tkhsh <y-takahashi@timedia.co.jp>
2016-02-03 01:32:45 +0900

    Answer ex-4.56

---
+++ ex-4.56.scm
@@ -0,0 +1,17 @@
+; a
+(and (supervisor ?person (Bitdiddle Ben))
+     (address    ?person ?where))
+
+; b
+(and (salary ?person ?amount)
+     (salary (Bitdiddle Ben) ?x)
+     (lisp-value < ?amount ?x))
+
+; c
+(and (supervisor ?person ?s)
+     (not (job ?s (computer . ?any)))
+     (job ?s ?pos))
+
+(and (job ?x ?j)
+     (not (job ?x (computer . ?any)))
+     (supervisor ?person ?x))
