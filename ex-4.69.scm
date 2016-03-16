acfe8a004fd2d7eb8cb8849ab5ad390cee651a0a
tkhsh <y-takahashi@timedia.co.jp>
2016-03-17 01:28:52 +0900

    Answer ex-4.69

---
+++ ex-4.69.scm
@@ -0,0 +1,39 @@
+(load "./ch4-query.scm")
+
+(define queries
+  '(
+    (assert! (son Adam Cain))
+    (assert! (son Cain Enoch))
+    (assert! (son Enoch Irad))
+    (assert! (son Irad Mehujael))
+    (assert! (son Mehujael Methushael))
+    (assert! (son Methushael Lamech))
+    (assert! (wife Lamech Ada))
+    (assert! (son Ada Jabal))
+    (assert! (son Ada Jubal))
+    (assert! (rule (grandchild ?g ?s)
+                   (and (son ?f ?s)
+                        (son ?g ?f))))
+    (assert! (rule (son ?m ?s)
+                   (and (wife ?m ?w)
+                        (son ?w ?s))))
+
+    (assert! (rule (end-in-grandchild (grandchild . ()))))
+    (assert! (rule (end-in-grandchild (?f . ?r))
+                   (end-in-grandchild ?r)))
+    ; (end-in-grandchild (great grandchild))
+    ; (end-in-grandchild (great great grandchild))
+
+    (assert! (rule ((great . ?rel) ?g ?d)
+                   (and (end-in-grandchild ?rel)
+                        (son ?g ?gc)
+                        (?rel ?gc ?d))))
+    (assert! (rule ((grandchild . ()) ?g ?gc)
+                   (grandchild ?g ?gc)))
+
+    ((great grandchild) ?g ?ggs)
+    ; 上記のクエリは結果が返るが、以下のクエリは返らない。
+    ; (?relationship Adam Irad)
+    ))
+
+(test-queries queries)
