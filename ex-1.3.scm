3a82ca4a2861449b6540cbdcf93fa37a61fc04e8
tkhsh <y-takahashi@timedia.co.jp>
2016-04-27 19:36:37 +0900

    ex-1.3: Fix

--- ex-1.3.scm
+++ ex-1.3.scm
@@ -5,11 +5,11 @@
   (+ (square x) (square y)))
 
 (define (f x y z)
-  (cond ((and (< x y) (< x z))
+  (cond ((and (<= x y) (<= x z))
          (sum-of-squares y z))
-        ((and (< y x) (< y z))
+        ((and (<= y x) (<= y z))
          (sum-of-squares x z))
-        ((and (< z x) (< z y))
+        ((and (<= z x) (<= z y))
          (sum-of-squares x y))))
 
 (define (main args)
