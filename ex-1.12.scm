17a9d99dfbd2cd8ac813908b273faf9a66e4202f
tkhsh <y-takahashi@timedia.co.jp>
2016-05-23 00:04:47 +0900

    Answer ex-1.12

---
+++ ex-1.12.scm
@@ -0,0 +1,18 @@
+(define (pascal-triangle x y)
+  (cond ((= x 1) 1)
+        ((edge? x y) 1)
+        (else (+ (pascal-triangle (- x 1) (- y 1))
+                 (pascal-triangle (- x 1) y)))))
+
+(define (edge? x y)
+  (cond ((= x y) #t)
+        ((= 1 y) #t)
+        (else #f)))
+
+(define (main args)
+  ; (print (pascal-triangle 1000 1))
+  (print (pascal-triangle 3 2))
+  (print (pascal-triangle 4 3))
+  (print (pascal-triangle 5 4))
+  (print (pascal-triangle 5 3))
+  )
